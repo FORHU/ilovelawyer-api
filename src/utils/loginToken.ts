7e16d54e5466c2493a951e78e713562ebe900eaa
joelralph <joelralph06@gmail.com>
2026-07-28 10:49:14 +0800

    chore: update expiresIn

--- src/utils/loginToken.ts
+++ src/utils/loginToken.ts
@@ -1,11 +1,18 @@
 import jwt from "jsonwebtoken";
-import { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRY, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRY_DAYS } from "../config";
+import {
+    ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRY,
+    REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRY_DAYS,
+} from "../config";
 
 export default function loginToken(userId: string, remember = false) {
-  const accessToken = jwt.sign({ userId }, ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRY });
-  const refreshToken = jwt.sign({ userId, remember }, REFRESH_TOKEN_SECRET, {
-    expiresIn: `${REFRESH_TOKEN_EXPIRY_DAYS}d`,
-  });
+    const accessToken = jwt.sign({ userId }, ACCESS_TOKEN_SECRET, {
+        expiresIn: ACCESS_TOKEN_EXPIRY,
+    });
+    const refreshToken = jwt.sign({ userId, remember }, REFRESH_TOKEN_SECRET, {
+        expiresIn: `${REFRESH_TOKEN_EXPIRY_DAYS}`,
+    });
 
-  return { accessToken, refreshToken };
+    return { accessToken, refreshToken };
 }
